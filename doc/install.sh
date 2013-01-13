3d39ffa15091eb9f37421c7e9af08db23123196e
Laszlo.simon <laszlo.simon@gmail.com>
2013-01-13 17:05:15 +0100

    lxterminal config

--- doc/install.sh
+++ doc/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Fuse without gtk from source:
 apt-get instal libspectrum-dev libspectrum8
 apt-get instal libsdl1.2-dev libaudiofile-dev
@@ -13,4 +14,13 @@ make install
 perl -i -pe 's/^.*framebuffer_width=\d*/framebuffer_width=640/' /boot/config.txt
 perl -i -pe 's/^.*framebuffer_height=\d*/framebuffer_height=480/' /boot/config.txt
 # run fuse: $ sudo fuse -g 2x
+
+# ZX themes:
+wget www.urbanfonts.com/download.php?file=zx81.zip
+unzip download.php\?file\=zx81.zip 
+mkdir /usr/share/fonts/truetype/zx
+cp zx*.ttf /usr/share/fonts/truetype/zx/
+chown root /usr/share/fonts/truetype/zx/*.ttf
+mkfontdir /usr/share/fonts/truetype/zx
+cp -R ../config/lxterminal ~/.config
 
